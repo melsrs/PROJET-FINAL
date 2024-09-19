bb9c723ec96dde34b3339272fa1474374fc2b326
Mélissa <melissa.soares1909@gmail.com>
2024-09-19 16:06:14 +0200

    modif footer

--- src/Views/Includes/footer.php
+++ src/Views/Includes/footer.php
@@ -1,3 +1,10 @@
+<div class="footer">
+    <a href="<?= HOME_URL . 'mentionsLegales' ?>">Mentions légales</a>
+    <a href="<?= HOME_URL . 'politiqueConfidentialité' ?>">Politique de confidentalité</a>
+    <a href="<?= HOME_URL . 'conditionGenerale' ?>">Conditions générales d'utilisation</a>
+    <p>Copyright © 2024 SNK. Tous droits réservés.</p>
+</div>
 
 </body>
-</html>
+
+</html
